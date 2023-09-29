0cebb8c20a61178d4f79d8e7b5f4333436eddde4
nani-samireddy <nanisamireddy05@gmail.com>
2023-09-30 01:16:14 +0530

    initial commit

---
+++ src/components/home/popular-course.data.ts
@@ -0,0 +1,29 @@
+import type { Course } from '@/interfaces/course'
+
+export const data: Array<Course> = [
+  {
+    id: 1,
+    cover: '/images/courses/custom-tshirt.jpeg',
+    title: 'Custom T-Shirt Design and Printing',
+
+    description: 'Custom T-Shirt Designing and Printing with industry level quality and experience. ',
+  },
+  {
+    id: 2,
+    cover: '/images/courses/a9e7b27a0c5e986a22416d79e2e9dba9.jpg',
+    title: 'Custom T-Shirt Design and Printing',
+    description: 'Custom T-Shirt Designing and Printing with industry level quality and experience. ',
+  },
+  {
+    id: 3,
+    cover: '/images/courses/a9e7b27a0c5e986a22416d79e2e9dba9.jpg',
+    title: 'Custom T-Shirt Design and Printing',
+    description: 'Custom T-Shirt Designing and Printing with industry level quality and experience. ',
+  },
+  {
+    id: 4,
+    cover: '/images/courses/a9e7b27a0c5e986a22416d79e2e9dba9.jpg',
+    title: 'Custom T-Shirt Design and Printing',
+    description: 'Custom T-Shirt Designing and Printing with industry level quality and experience. ',
+  },
+]
