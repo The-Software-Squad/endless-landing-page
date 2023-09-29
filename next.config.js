340b8a1bba5af9aed5c849a39d6c75ae30803e2b
nani-samireddy <nanisamireddy05@gmail.com>
2023-09-30 01:38:12 +0530

    updated the config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/*',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
